19f5f17a4f83957c5ec1d5e9b8a44acd047a1dc9
victor <vgarzom@gmail.com>
2019-09-27 01:14:47 -0500

    creates application

--- colors.js
+++ colors.js
@@ -11,17 +11,17 @@
  * @param   Number  b       The blue color value
  * @return  Array           The HSL representation
  */
-function rgbToHsl(r, g, b){
+function rgbToHsl(r, g, b) {
     r /= 255, g /= 255, b /= 255;
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
     var h, s, l = (max + min) / 2;
 
-    if(max == min){
+    if (max == min) {
         h = s = 0; // achromatic
-    }else{
+    } else {
         var d = max - min;
         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
+        switch (max) {
             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
             case g: h = (b - r) / d + 2; break;
             case b: h = (r - g) / d + 4; break;
@@ -43,26 +43,26 @@ function rgbToHsl(r, g, b){
  * @param   Number  l       The lightness
  * @return  Array           The RGB representation
  */
-function hslToRgb(h, s, l){
+function hslToRgb(h, s, l) {
     var r, g, b;
 
-    if(s == 0){
+    if (s == 0) {
         r = g = b = l; // achromatic
-    }else{
-        function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    } else {
+        function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
             return p;
         }
 
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
+        r = hue2rgb(p, q, h + 1 / 3);
         g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
+        b = hue2rgb(p, q, h - 1 / 3);
     }
 
     return [r * 255, g * 255, b * 255];
@@ -79,18 +79,18 @@ function hslToRgb(h, s, l){
  * @param   Number  b       The blue color value
  * @return  Array           The HSV representation
  */
-function rgbToHsv(r, g, b){
-    r = r/255, g = g/255, b = b/255;
+function rgbToHsv(r, g, b) {
+    r = r / 255, g = g / 255, b = b / 255;
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
     var h, s, v = max;
 
     var d = max - min;
     s = max == 0 ? 0 : d / max;
 
-    if(max == min){
+    if (max == min) {
         h = 0; // achromatic
-    }else{
-        switch(max){
+    } else {
+        switch (max) {
             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
             case g: h = (b - r) / d + 2; break;
             case b: h = (r - g) / d + 4; break;
@@ -112,7 +112,7 @@ function rgbToHsv(r, g, b){
  * @param   Number  v       The value
  * @return  Array           The RGB representation
  */
-function hsvToRgb(h, s, v){
+function hsvToRgb(h, s, v) {
     var r, g, b;
 
     var i = Math.floor(h * 6);
@@ -121,7 +121,7 @@ function hsvToRgb(h, s, v){
     var q = v * (1 - f * s);
     var t = v * (1 - (1 - f) * s);
 
-    switch(i % 6){
+    switch (i % 6) {
         case 0: r = v, g = t, b = p; break;
         case 1: r = q, g = v, b = p; break;
         case 2: r = p, g = v, b = t; break;
@@ -132,4 +132,7 @@ function hsvToRgb(h, s, v){
 
     return [r * 255, g * 255, b * 255];
 }
-	
+
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
--- script.js
+++ script.js
@@ -1,9 +1,59 @@
-function randomPalette(){
+var demo_elements, colors_count, color_separation;
 
+function init() {
+  demo_elements = [
+    "website-background",
+    "element-text",
+    "element-border",
+    "element-background",
+    "header"
+  ]
+  colors_count = demo_elements.length;
+  color_separation = 360 / colors_count;
+}
+
+function randomPalette() {
+  let init = Math.floor(Math.random() * 359);
+  let colors = [];
+  for (let i = 0; i < colors_count; i++) {
+    let ccolor = init + (i * color_separation);
+    if (ccolor > 359) {
+      ccolor = ccolor - 359;
+    }
+    colors[i] = hsvToRgb(ccolor / 360, 1, 1);
+    colors[i].push(ccolor)
+  }
+  return colors;
+  //console.log("A ver!", init, colors);
 }
 
 
 
-function generateRules(){
+function generateRules() {
+  let colors = randomPalette();
+  let rules = "";
+  colors.map((c, i) => {
+    let r = Math.floor(colors[i][0]);
+    let g = Math.floor(colors[i][1]);
+    let b = Math.floor(colors[i][2]);
+    let hex = rgbToHex(r, g, b);
+    $("#" + demo_elements[i]).css({ 'background-color': hex });
+    rules = rules + `.${demo_elements[i]}{color: ${hex};}\n`;
+    if (colors[i][3] > 38 && colors[i][3] < 210) {
+      $("#" + demo_elements[i]).css({ 'color': `black` });
+    } else {
+      $("#" + demo_elements[i]).css({ 'color': `white` });
+    }
+  })
+  $("#css-rules").val(rules);
+}
 
+function clearPalette() {
+  let rules = "";
+  demo_elements.map(e => {
+    rules = rules + `.${e}{color: #ffffff;}\n`;
+    $("#" + e).css({ 'background-color': `#F3F6FA` });
+    $("#" + e).css({ 'color': `black` });
+  });
+  $("#css-rules").val(rules);
 }
